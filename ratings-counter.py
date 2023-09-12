e89ece46d08a42bd5810dd64cbfa6cffcd1ce3f4
Brian Addison <baddison@likewise.com>
2023-09-11 21:32:26 -0700

    some descriptions
    of stuff

--- ratings-counter.py
+++ ratings-counter.py
@@ -1,11 +1,27 @@
 from pyspark import SparkConf, SparkContext
 import collections
 
+# Configure
+#   set the master node as the local machine
+#   set an App Name
 conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
+
+# Create the spark context
+#   by convention the SparkContext is assigned to variable sc
 sc = SparkContext(conf=conf)
 
+# load the data
+#   sc.textFile will create an RDD from the u.data file
 lines = sc.textFile("ml-100k/u.data")
+
+# Extract aka map the data we care about. store it in ratings
+#   ratings is also an RDD
 ratings = lines.map(lambda x: x.split()[2])
+
+# Perform action: Count by value
+#   this feels like a SQL GROUP BY. grouping by the rating
+#   and COUNT(*)
+#   result is a python object, not an RDD
 result = ratings.countByValue()
 
 sortedResults = collections.OrderedDict(sorted(result.items()))
